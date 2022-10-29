360104853cbe032a4a4a5123155ad29e94bd8141
roei book <rbook@g.jct.ac.il>
2022-10-29 23:26:37 +0300

    initial code + welcome functions

---
+++ Stage0/Program0102.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0102();
+            Wellcome4289();
+            Console.ReadKey();
+        }
+
+        private static void Welcome0102()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Wellcome4289();
+    }
+}
+
+
+
