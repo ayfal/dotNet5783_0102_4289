ccf75ef0c3f8162dc1c8495dee93a9c1d7b0a318
roei book <rbook@g.jct.ac.il>
2022-11-06 18:49:25 +0200

    entites

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -5,4 +5,20 @@ namespace DO;
 /// </summary>
 internal class OrderItem
 {
+    /// <summary>
+    /// Unique ProductID for
+    /// </summary>
+    public int ProductID { get; set; }
+    /// <summary>
+    /// Unique OrderID for
+    /// </summary>
+    public int OrderID { get; set; }
+    /// <summary>
+    /// Unique Price for
+    /// </summary>
+    public double Price { get; set; }
+    /// <summary>
+    /// Unique Amount for
+    /// </summary>
+    public int Amount { get; set; }    
 }
