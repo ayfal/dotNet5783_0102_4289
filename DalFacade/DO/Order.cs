3260e213be9bf4ec7879765ef9c0e85f9513929e
ariel falik <arielfalik@gmail.com>
2022-11-06 18:52:21 +0200

    entities 2nd try

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -6,4 +6,33 @@ namespace DO;
 /// </summary>
 public struct Order
 {
+    /// <summary>
+    /// Uniqe ID of order
+    /// </summary>
+    public int ID { get; set; }
+    /// <summary>
+    /// Customer's name
+    /// </summary>
+    public string CustomerName { get; set; }
+    /// <summary>
+    /// Customer's Email
+    /// </summary>
+    public string CustomerEmail { get; set; }
+    /// <summary>
+    /// Customer's address
+    /// </summary>
+    public string CustomerAddress { get; set; }
+    /// <summary>
+    /// Order's date
+    /// </summary>
+    public DateTime OrderDate { get; set; }
+    /// <summary>
+    /// Shipping's date
+    /// </summary>
+    public DateTime ShipDate { get; set; }
+    /// <summary>
+    /// Delivery's date
+    /// </summary>
+    public DateTime DeliveryDate { get; set; }
+
 }
