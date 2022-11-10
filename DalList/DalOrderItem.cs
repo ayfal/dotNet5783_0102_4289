0ca50b13c0f9f967583cf5027d1e9cb4a610aaaf
ariel falik <arielfalik@gmail.com>
2022-11-10 17:36:42 +0200

    most of dal and a bit of main

--- DalList/DalOrderItem.cs
+++ DalList/DalOrderItem.cs
@@ -1,9 +1,43 @@
-﻿namespace Dal;
+﻿using DO;
+using static Dal.DataSource;
 
-<<<<<<< HEAD
+namespace Dal;
+
 public class DalOrderItem
-=======
-internal class DalOrderItem
->>>>>>> 3d92f4de615ca244e195f52fab0beac5600576db
 {
+    public int Add(OrderItem orderItem)
+    {
+        orderItem.ID = Config.orderItemId;//check what happens with config.orderID
+        orderItems[Config.orderItems] = orderItem;
+        Config.orderItems++;
+        return orderItem.ID;
+    }
+    public void Delete(int ID)
+    {
+        Get(ID);
+        orderItems = orderItems.Where(i => i.ID != ID).ToArray();
+        Config.orderItems--;
+    }
+    public void Update(Order orderItem)
+    {
+        Get(orderItem.ID);
+        for (int i = 0; i < Config.orderItems; i++)
+        {
+            if (orderItems[i].ID == orderItem.ID) orders[i] = orderItem;
+        }
+    }
+    public OrderItem Get(int ID)
+    {
+        for (int i = 0; i < Config.orderItems; i++)
+        {
+            if (orderItems[i].ID == ID) return orderItems[i];
+        }
+        throw new Exception("key not found");
+    }
+    public OrderItem[] Get()
+    {
+        return orderItems.ToArray();
+    }
+
+    //TODO add more methods from the general description file. all should be public
 }
--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace Dal;//I guess
+class Program
+{
+    private DalProduct dalProduct = new DalProduct();
+    private DalOrder dalOrder = new DalOrder();
+    private DalOrderItem dalOrderItem = new DalOrderItem();
+
+    void main()
+    {
+        int option;
+        bool success;
+        do
+        {
+            Console.WriteLine("Welcome to the test menu\n" +
+                "Please choose one of the following options:\n" +
+                "keep in mind to use valid data only\n" +
+                "0. Exit\n" +
+                "1. Check Products\n" +
+                "2. Check Orders\n" +
+                "3. Check Order Items");
+            success = int.TryParse(Console.ReadLine(), out option);
+            switch (option)
+            {
+                case 0:
+
+                default:
+                    if (!(success && (option == 0))) Console.WriteLine("Bad command! Go stand in the corner!");
+                    break;
+            }
+        } while (success && (option==0));
+
+    }
+}
