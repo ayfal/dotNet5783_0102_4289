3f09533de86ee7e59cfc4232d29ba62f298a38c6
roei book <rbook@g.jct.ac.il>
2022-11-23 14:41:54 +0200

    start part 7 stage 2

---
+++ BL/BlImplementation/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+using DalApi;
+using Dal;
+
+namespace BlImplementation
+{
+    internal class Cart : ICart
+    {
+        private IDal Dal = new DalList();
+        public BO.Cart Add(BO.Cart cart, int productID)
+        {
+            try
+            {
+                var item = cart.Items.First(p => p.ID == productID);
+                if (Dal._product.Get(productID).InStock == 0)
+                    throw new Exception("out of stock");
+                else
+                    item.Amount++; 
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        public Cart UpdateAmount(Cart cart, int productID, int amount);
+        public void Checkout(Cart cart, string customerName, string Email, string address);
+
+    }
+}
