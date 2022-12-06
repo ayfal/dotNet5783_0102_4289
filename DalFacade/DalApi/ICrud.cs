f101ec05493341dc7e6b0c268c40790da9af796b
ariel falik <arielfalik@gmail.com>
2022-12-06 13:20:15 +0200

    add predicate delgate in ICrud

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -11,7 +11,7 @@ namespace DalApi
         int Add(T t);
         void Delete(int ID);
         void Update(T t);
-        IEnumerable<T?> Get();
+        IEnumerable<T?> Get(Func<T?, bool>? f=null);
         T Get(int ID);
 
     }
